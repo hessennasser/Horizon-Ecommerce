22c2d74dfa1cc7ad3843248403bd11a672dfff6a
Hessen Nasser <92742551+hessennasser@users.noreply.github.com>
2023-09-08 15:29:19 +0300

    Add files via upload

---
+++ assets/styles-0d5915be.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-dc16dc37.js";import{l as e}from"./horizon-logo-035e306d.js";const o=()=>s.jsx("div",{className:"flex items-center justify-center flex-col text-white py-10",children:s.jsx("img",{className:"w-44",src:e,alt:"horizon"})});export{o as B};
